cf0eb83d1e6c6c94a0f553f17de3b476caefcbd0
austin <austinhuang1203@gmail.com>
2022-10-09 00:38:36 +0800

    synced api

--- code.js
+++ code.js
@@ -13,20 +13,61 @@ async function ??() {
 
 const getweather = (() => {
     function initialise(loc){
-        const sky = document.getElementById("sky");
-        async function getsky() {
+        const weather1 = document.getElementById("weather1");
+        const city1 = document.getElementById("city1");
+        const temp1 = document.getElementById("temp1");
+        const temp2 = document.getElementById("temp2");
+        const humidity1 = document.getElementById("humidity1");
+        const rain1 = document.getElementById("rain1");
+        const windspeed1 = document.getElementById("wind1");
+        async function getdata() {
         
             
             const response = await fetch('https://api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=f754492e8565556b51c677717f4cb2f8',{mode: 'cors'});
             const weatherdata = await response.json();
-            sky.innerHTML = weatherdata.weather[0].description;
+            console.log("run warning");
+            weather1.innerHTML = weatherdata.weather[0].description.charAt(0).toUpperCase() +weatherdata.weather[0].description.slice(1);
+            city1.innerHTML = weatherdata.name;
+            temp1.innerHTML = parseInt(weatherdata.main.temp - 273.15) + "°C";
+            temp2.innerHTML = parseInt(weatherdata.main.feels_like - 273.15) + "°C";
+            humidity1.innerHTML = weatherdata.main.humidity + "%";
+            rain1.innerHTML = weatherdata.main.pressure + "hPa";
+            windspeed1.innerHTML = weatherdata.wind.speed + "m/s";
         }
-        getsky();
+        getdata();
     }
 
     return {initialise};
 
 })();
+
+const initialise = (() => {
+
+    function start1(){
+        getweather.initialise("London");
+        document.getElementById("search1").addEventListener("keydown",function (e) {
+            if (e.key === 'Enter') {
+            submit();
+
+            }
+        })
+    }
+
+    function submit(){
+        const search1 = document.getElementById("search1");
+        if (search1.value == ""){
+            return;
+        }
+        else{
+            console.log("h,");
+            search1.value = "";
+        }
+    }
+
+    return {start1};
+})();
+
+initialise.start1();
 
 //getweather.initialise("London");
 
